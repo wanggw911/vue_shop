445eedc2245c73355002be0f79606904f43ac552
wanggw911 <wangguowen911@gmail.com>
2020-03-06 17:31:56 +0800

    左侧菜单栏的折叠展开功能

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -8,14 +8,14 @@
             </div>
             <el-button type="info" @click="logout">退出</el-button>
         </el-header>
+        <!-- 页面主体区域 -->
         <el-container>
-            <!-- 左边试图 -->
+            <!-- 左边侧边栏 -->
             <el-aside width="200px">
-                <el-menu
-                    :unique-opened="true"
-                    background-color="#333744"
-                    text-color="#fff"
-                    active-text-color="#409EFF">
+                <!-- 下面的代码貌似有点问题。。。 -->
+            <!-- <el-aside width="isCollapse ? '64px' : '200px'"> -->
+                <dir class="toggle-button" @click="toggleCollapse">|||</dir>
+                <el-menu :collapse="isCollapse" :collapse-transition="false" :unique-opened="true" background-color="#333744" text-color="#fff" active-text-color="#409EFF">
                     <!-- 一级菜单模板区 -->
                     <el-submenu :index="item.id + ''" v-for="item in menuList" :key="item.id">
                         <!-- 一级菜单模版区域 -->
@@ -43,6 +43,7 @@
 export default {
     data() {
         return {
+            isCollapse: false,
             menuList: [],
             iconsObject: {
                 '1': 'iconfont icon-user',
@@ -58,6 +59,10 @@ export default {
         this.getMenuList()
     },
     methods:{
+        toggleCollapse() {
+            //点击按钮，菜单的折叠和展开
+            //this.isCollapse = !this.isCollapse
+        },
         logout() {
             this.$message.success('退出登录成功');
             window.sessionStorage.clear();
@@ -177,4 +182,17 @@ export default {
 .iconfont{
     margin-right: 10px;
 }
+
+.toggle-button{
+    background-color: #4a5064;
+    font-size: 10px;
+    line-height: 24px;
+    color: #fff;
+    text-align: center;
+    letter-spacing: 0.2em;//让文字之间间距增大
+    cursor: pointer; //设置鼠标放上去，变成一只小手
+    margin: 0;//好奇怪，为啥会有上下 mergin，导致上下有间距
+    padding: 0;//好奇怪，为啥有 padding，导致文字不能居中
+}
+
 </style>
