802383300f7f6e7998b0810e1c578e21555ada3b
wanggw911 <wangguowen911@gmail.com>
2020-03-06 15:20:30 +0800

    element-ui布局进行主页的布局

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,7 +1,17 @@
 <template>
-    <div class="home_container">
-        <el-button type="info" @click="logout">退出</el-button>
-    </div>
+    <el-container class="home-container">
+        <!-- 头部试图 -->
+        <el-header>
+            Header
+            <el-button type="info" @click="logout">退出</el-button>
+        </el-header>
+        <el-container>
+            <!-- 左边试图 -->
+            <el-aside width="200px">Aside</el-aside>
+            <!-- 右边主视图 -->
+            <el-main>Main</el-main>
+        </el-container>
+    </el-container>
 </template>
 
 <script>
@@ -17,4 +27,20 @@ export default {
 </script>
 
 <style lang="less" scoped>
+
+.home-container{
+    height: 100%;
+}
+
+.el-header{
+    background-color: #373D41;
+}
+
+.el-aside{
+    background-color: #333744;
+}
+
+.el-main{
+    background-color: #eaedf1;
+}
 </style>
--- src/plugins/element.js
+++ src/plugins/element.js
@@ -1,9 +1,13 @@
 import Vue from 'vue'
-import { Button, Form, FormItem, Input, Message } from 'element-ui'
+import { Button, Form, FormItem, Input, Message, Container, Header, Aside, Main } from 'element-ui'
 
 // 注册成全局可以用的组件
 Vue.use(Button)
 Vue.use(Form)
 Vue.use(FormItem)
 Vue.use(Input)
+Vue.use(Container)
+Vue.use(Header)
+Vue.use(Aside)
+Vue.use(Main)
 Vue.prototype.$message = Message
