9056f6facf13c252fc33e50d57bd68d51ffb9c9f
wanggw911 <wangguowen911@gmail.com>
2020-03-06 14:42:45 +0800

    添加路由导航守卫控制访问权限

--- src/router/index.js
+++ src/router/index.js
@@ -15,4 +15,14 @@ const router = new VueRouter({
   routes
 })
 
+// 挂载路由导航守卫
+router.beforeEach((to, from, next) => {
+  if (to.path == '/login') return next()
+
+  const token = window.sessionStorage.getItem('token')
+  if (!token) return next('/login')
+
+  next()
+})
+
 export default router
