b71ec309507684b817e27b9554449cc8987579b1
wanggw911 <wangguowen911@gmail.com>
2020-03-06 19:00:00 +0800

    用户列表界面面包屑导航以及搜索栏布局添加

--- src/components/user/Users.vue
+++ src/components/user/Users.vue
@@ -1,6 +1,28 @@
 <template>
     <div>
-        <h3>用户列表组件</h3>
+        <!-- 面包屑 导航区 -->
+        <el-breadcrumb separator-class="el-icon-arrow-right">
+            <el-breadcrumb-item :to="{ path: '/home' }">首页</el-breadcrumb-item>
+            <el-breadcrumb-item>活动管理</el-breadcrumb-item>
+            <el-breadcrumb-item>活动列表</el-breadcrumb-item>
+        </el-breadcrumb>
+
+        <!-- 卡片视图 -->
+        <el-card class="box-card">
+            <!-- 搜索与添加按钮区域 -->
+            <el-row gutter="20"> 
+                <!-- gutter 设置每个格子之间的间距 -->
+                <!-- 使用列布局，来避免整个输入框占满一行 -->
+                <el-col :span="10">
+                    <el-input placeholder="请输入内容" v-model="input3" class="input-with-select">
+                        <el-button slot="append" icon="el-icon-search"></el-button>
+                    </el-input>
+                </el-col>
+                <el-col :span="4">
+                    <el-button type="primary">添加用户</el-button>
+                </el-col>
+            </el-row>
+        </el-card>
     </div>
 </template>
 
--- src/plugins/element.js
+++ src/plugins/element.js
@@ -1,5 +1,11 @@
 import Vue from 'vue'
-import { Button, Form, FormItem, Input, Message, Container, Header, Aside, Main, Menu, Submenu, MenuItem } from 'element-ui'
+import { 
+    Button, Form, FormItem, Input, Message, Container, Header, Aside, 
+    Main, Menu, Submenu, MenuItem, 
+    Breadcrumb, BreadcrumbItem, 
+    Card,
+    Row, Col
+} from 'element-ui'
 
 // 注册成全局可以用的组件
 Vue.use(Button)
@@ -14,5 +20,13 @@ Vue.use(Main)
 Vue.use(Menu)
 Vue.use(Submenu)
 Vue.use(MenuItem)
+
+Vue.use(Breadcrumb)
+Vue.use(BreadcrumbItem)
+
+Vue.use(Card)
+
+Vue.use(Row)
+Vue.use(Col)
 
 Vue.prototype.$message = Message
