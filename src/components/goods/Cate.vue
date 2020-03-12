ad010bf0d04047f29ba2293240d4891b8f86928a
wanggw911 <wangguowen911@gmail.com>
2020-03-12 16:13:40 +0800

    检查请求成功码，删除无用打印

--- src/components/goods/Cate.vue
+++ src/components/goods/Cate.vue
@@ -199,19 +199,13 @@ export default {
             this.parentCateList = res.data
         },
         addCateAction () {
-            console.log('addCateAction')
-            console.log(this.addCateForm)
-
              this.$refs.addCateFormRef.validate(async valid => {
                  if(!valid) return 
 
                  const {data: res} = await this.$http.post('categories', this.addCateForm)
-                 console.log(res)
-                 if (res.meta.status !== 200) {
-                    console.log('addCateAction 添加商品分类失败！')
+                 if (res.meta.status !== 201) {
                     return this.$message.error('添加商品分类失败！')
                  }
-                 console.log('addCateAction 添加商品分类成功！')
                  this.$message.success('添加商品分类成功！')
 
                  this.getCategoryList()
