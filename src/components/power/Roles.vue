fa5fca1c23f0c8d7786e1633d40a0922d54bf795
wanggw911 <wangguowen911@gmail.com>
2020-03-12 10:02:39 +0800

    完成删除权限标签的功能

--- src/components/power/Roles.vue
+++ src/components/power/Roles.vue
@@ -26,7 +26,7 @@
                         <el-row :class="['vcenter', 'bdbottom', i1 === 0 ? 'bdtop' : '']" v-for="(item1, i1) in scope.row.children" :key="item1.id">
                             <!-- 渲染一级权限，总共分为24列，前面5列，后面19列 -->
                             <el-col :span="5">
-                                <el-tag>{{item1.authName}}</el-tag>
+                                <el-tag  closable @close="removeRightById(scope.row, item1.id)">{{item1.authName}}</el-tag>
                                 <!-- 小箭头组件 -->
                                 <i class="el-icon-caret-right"></i>
                             </el-col>
@@ -35,12 +35,12 @@
                                 <!-- 通过 for 循环 嵌套渲染二级权限 -->
                                 <el-row :class="['vcenter', i2 === 0? '' :'bdtop']" v-for="(item2, i2) in item1.children" :key="item2.id">
                                     <el-col :span="6">
-                                        <el-tag type="success">{{item2.authName}}</el-tag>
+                                        <el-tag type="success" closable @close="removeRightById(scope.row, item2.id)">{{item2.authName}}</el-tag>
                                         <i class="el-icon-caret-right"></i>
                                     </el-col>
                                     <el-col :span="18">
                                         <!-- 渲染出三级结构 -->
-                                        <el-tag type="warning" v-for="(item3) in item2.children" :key="item3.id">
+                                        <el-tag v-for="(item3) in item2.children" :key="item3.id" type="warning" closable @close="removeRightById(scope.row, item3.id)">
                                             {{item3.authName}}
                                         </el-tag>
                                     </el-col>
@@ -90,6 +90,35 @@ export default {
             this.rolesList = res.data
             console.log('获取角色列表成功！')
             console.log(res.data)
+        },
+        // 根据id删除对应的权限
+        async removeRightById(role, rightId) {
+            //弹框提示用户是否需要删除
+            const confirmResult = await this.$confirm('此操作将永久删除该标签, 是否继续?', '提示', {
+                confirmButtonText: '确定',
+                cancelButtonText: '取消',
+                type: 'warning'
+            }).catch(error => error)
+            
+            console.log(confirmResult)
+            if (confirmResult !== 'confirm') {
+                return this.$message.info('已取消删除')
+            }
+            console.log('确认了删除')
+
+            //删除用户的网络请求
+            const {data: res} = await this.$http.delete(`roles/${role.id}/rights/${rightId}`)
+            if (res.meta.status !== 200) {
+                console.log('删除标签失败')
+                return this.$message.error('删除标签失败')
+            }
+            console.log('删除标签成功')
+            this.$message.success('删除标签成功')
+
+            //重新加载数据会导致，页面渲染
+            //this.getRolesList()
+            //直接设置数据，局部刷新
+            role.children = res.data
         },
     }
 }
