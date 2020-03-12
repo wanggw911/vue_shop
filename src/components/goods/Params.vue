2f9327b67d1675c67eb6ab57afc2a24702b8c117
wanggw911 <wangguowen911@gmail.com>
2020-03-12 22:31:27 +0800

    完成了参数的删除功能

--- src/components/goods/Params.vue
+++ src/components/goods/Params.vue
@@ -40,7 +40,7 @@
                         <el-table-column label="操作" width="180px">
                             <template slot-scope="scope">
                                 <el-button type="primary" icon="el-icon-edit" size="mini" @click="showEditDialog(scope.row)">编辑</el-button>
-                                <el-button type="danger" icon="el-icon-delete" size="mini" @click="removeUserById(scope.row.id)">删除</el-button>
+                                <el-button type="danger" icon="el-icon-delete" size="mini" @click="removeParamById(scope.row.attr_id)">删除</el-button>
                             </template>
                         </el-table-column>
                     </el-table>
@@ -61,7 +61,7 @@
                         <el-table-column label="操作" width="180px">
                             <template slot-scope="scope">
                                 <el-button type="primary" icon="el-icon-edit" size="mini" @click="showEditDialog(scope.row)">编辑</el-button>
-                                <el-button type="danger" icon="el-icon-delete" size="mini" @click="removeUserById(scope.row.id)">删除</el-button>
+                                <el-button type="danger" icon="el-icon-delete" size="mini" @click="removeParamById(scope.row.attr_id)">删除</el-button>
                             </template>
                         </el-table-column>
                     </el-table>
@@ -261,6 +261,26 @@ export default {
                  this.getAttributesData()
              })
         },
+        async removeParamById(attr_id) {
+            const confirmResult = await this.$confirm('此操作将永久删除该参数, 是否继续?', '提示', {
+                confirmButtonText: '确定',
+                cancelButtonText: '取消',
+                type: 'warning'
+            }).catch(error => error)
+            
+            console.log(confirmResult)
+            if (confirmResult !== 'confirm') {
+                return this.$message.info('已取消删除')
+            }
+
+            //删除用户的网络请求
+            const {data: res} = await this.$http.delete(`categories/${this.cateId}/attributes/${attr_id}`)
+            if (res.meta.status !== 200) {
+                return this.$message.error('删除参数失败')
+            }
+            this.$message.success('删除参数成功')
+            this.getAttributesData()
+        }
     }
 }
 </script>
