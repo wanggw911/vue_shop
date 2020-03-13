b9822b9a3bd0e7acaef5ccfb7abd8adce8bbbce9
wanggw911 <wangguowen911@gmail.com>
2020-03-13 10:40:35 +0800

    完成步骤条和tab栏的联动效果

--- src/components/goods/Add.vue
+++ src/components/goods/Add.vue
@@ -14,7 +14,7 @@
             </el-alert>
             
             <!-- 步骤条区域 -->
-            <el-steps :space="200" :active="activeIndex" finish-status="success" align-center>
+            <el-steps :space="200" :active="activeIndex - 0" finish-status="success" align-center>
                 <el-step title="基本信息"></el-step>
                 <el-step title="商品参数"></el-step>
                 <el-step title="商品属性"></el-step>
@@ -24,12 +24,13 @@
             </el-steps>
 
             <!-- 左侧Tab栏区域 -->
-            <el-tabs :tab-position="'left'" style="height: 200px;">
-                <el-tab-pane label="基本信息">基本信息</el-tab-pane>
-                <el-tab-pane label="商品参数">商品参数</el-tab-pane>
-                <el-tab-pane label="商品属性">商品属性</el-tab-pane>
-                <el-tab-pane label="商品照片">商品照片</el-tab-pane>
-                <el-tab-pane label="商品内容">商品内容</el-tab-pane>
+            <!-- style="height: 200px;" -->
+            <el-tabs :tab-position="'left'" v-model="activeIndex">
+                <el-tab-pane label="基本信息" name="0">基本信息</el-tab-pane>
+                <el-tab-pane label="商品参数" name="1">商品参数</el-tab-pane>
+                <el-tab-pane label="商品属性" name="2">商品属性</el-tab-pane>
+                <el-tab-pane label="商品照片" name="3">商品照片</el-tab-pane>
+                <el-tab-pane label="商品内容" name="4">商品内容</el-tab-pane>
             </el-tabs>
         </el-card>
     </div>
@@ -39,7 +40,7 @@
 export default {
     data() {
         return {
-            activeIndex: 0,
+            activeIndex: '0',
         }
     },
     created() {
