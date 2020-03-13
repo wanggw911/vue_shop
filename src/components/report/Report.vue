962eb025da84c9dad7524ba0c55df1872a86e762
wanggw911 <wangguowen911@gmail.com>
2020-03-13 21:25:56 +0800

    加载报表数据并渲染

--- src/components/report/Report.vue
+++ src/components/report/Report.vue
@@ -18,11 +18,39 @@
 <script>
 //1、导入echarts
 import echarts from 'echarts'
+//2、使用 lodash 合并两个对象
+import _ from 'lodash'
 
 export default {
     data() {
         return {
-            
+            //需要合并的数据
+            options: {
+                title: {
+                    text: '用户来源'
+                },
+                tooltip: {
+                    trigger: 'axis',
+                    axisPointer: {
+                        type: 'cross',
+                        label: {
+                        backgroundColor: '#E9EEF3'
+                        }
+                    }
+                },
+                grid: {
+                    left: '3%',
+                    right: '4%',
+                    bottom: '3%',
+                    containLabel: true
+                },
+                xAxis: [
+                    { boundaryGap: false }
+                ],
+                yAxis: [
+                    { type: 'value' }
+                ]
+            }
         }
     },
     created() {
@@ -30,10 +58,16 @@ export default {
         //this.getCategoryList()
     },
     //此时，页面上的元素，已经被渲染完毕
-    mounted() {
+    async mounted() {
+        // 获取折线图的数据
+        const {data: res} = await this.$http.get('reports/type/1')
+        if (res.meta.status !== 200) {
+            return this.$http.error('获取折线图数据失败'+res.meta.msg)
+        }
+
         // 3、基于准备好的dom，初始化echarts实例
         var myChart = echarts.init(document.getElementById('main'))
-// 指定图表的配置项和数据
+        // 指定图表的配置项和数据
         var option = {
             title: {
                 text: 'ECharts 入门示例'
@@ -54,7 +88,10 @@ export default {
         };
 
         // 使用刚指定的配置项和数据显示图表。
-        myChart.setOption(option);
+        //myChart.setOption(option)
+        //myChart.setOption(res.data)
+        const result = _.merge(res.data, this.options)
+        myChart.setOption(result)
     },
     methods: {
         // 获取权限列表
