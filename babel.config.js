84b9421f300d9c4d36f6907365614d28df578e50
wanggw911 <wangguowen911@gmail.com>
2020-03-22 08:56:29 +0800

    添加插件安装说明

--- babel.config.js
+++ babel.config.js
@@ -1,6 +1,8 @@
 
 const prodPlugins = []
 if (process.env.MODE_ENV === 'production') {
+  //https://babeljs.io/docs/en/babel-plugin-transform-remove-console
+  //安装：npm install babel-plugin-transform-remove-console --save-dev
   prodPlugins.push('transform-remove-console')
 }
 
@@ -16,9 +18,7 @@ module.exports = {
         styleLibraryName: 'theme-chalk'
       }
     ],
-    //上线打开，开发时禁用
-    //'transform-remove-console',
-    //发布产品时候的插件数组
+    //发布产品时候的插件数组，上线打开，开发时禁用
     ...prodPlugins,
   ]
 }
